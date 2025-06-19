2b6e820891c8a0984a7773d1085440b06ec20e27
Varshaleena <231401122@rajalakshmi.edu.in>
2025-06-19 11:25:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+  const message = document.getElementById("message");
+  message.textContent = "🎉 You just learned how to use JavaScript! 🎉";
+  message.classList.remove("hidden");
+}
